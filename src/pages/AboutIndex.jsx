32a532680e7cba381da8d998bb011f1481c24764
botirovdev <botirovdev7@gmail.com>
2024-01-11 21:27:32 +0500

    next first

--- src/pages/AboutIndex.jsx
+++ src/pages/AboutIndex.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Form from './Form'
 import About from './About'
+import Footer from '../layout/footer/Footer'
 
 function AboutIndex() {
   return (
     <div>
       <About/>
       <Form/>
+      <Footer/>
         
     </div>
   )
